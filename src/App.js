ac3c1b7b7f93705caf4e17a742e84cbd03d4dadd
Rony Quispe <oghmand@gmail.com>
2022-11-13 13:20:58 -0500

    Set users data in html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import logo from './logo.svg'
 
 function App() {
+
+  const [user, setUser] = useState({});
+
+  const getUser = async () => {
+    const data = await fetch(' https://api.github.com/users/sammwyy').then( response => response.json());
+    setUser(data);
+  }
+
+  useEffect( () => {
+    getUser();
+  },[])
+
   return (
     <div className="App">
       <div className="card">
         <div className="photo-profile">
-            <img src={logo} alt="profile" />
+            <img src={user?.avatar_url} alt="profile" />
         </div>
         <div className="content-profile">
-            <div className="name">Rony quispe</div>
-            <div className="bio">Bachiller Ingenieria de Computacion y Sistemas en la universidad San Martin de Porres, Lima-Perú</div>
+            <div className="name">{user?.name}</div>
+            <div className="bio">{user?.bio}</div>
             <div className="recounts">
                 <div className="followers">
-                    1,500
+                    {user?.followers}
                 </div>
                 <div className="repos">
-                    20
+                    {user?.public_repos}
                 </div>
-                <div className="twitter">
-                    @rquispeq
-                </div>
+                {(user?.twitter_username) && (<div className="twitter">
+                    <a target="__blank" href={'https://twitter.com/' + user?.twitter_username}>@{user?.twitter_username}</a>
+                </div>)}
+                
             </div>
         </div>
       </div>
