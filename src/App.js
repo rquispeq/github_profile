ca572bf46a1a99ac6431e2e1961c8d5ddf0abe50
Rony Quispe <oghmand@gmail.com>
2022-11-13 12:20:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-// import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+import logo from './logo.svg'
 
 function App() {
-  const logo = null
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="photo-profile">
+            <img src={logo} alt="profile" />
+        </div>
+        <div className="content-profile">
+            <div className="name">Rony quispe</div>
+            <div className="bio">Bachiller Ingenieria de Computacion y Sistemas en la universidad San Martin de Porres, Lima-Perú</div>
+            <div className="recounts">
+                <div className="followers">
+                    1,500
+                </div>
+                <div className="repos">
+                    20
+                </div>
+                <div className="twitter">
+                    @rquispeq
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
