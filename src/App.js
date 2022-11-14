c0eba26d936e5d0a7928bab72dad049c86c03137
Rony Quispe <oghmand@gmail.com>
2022-11-13 20:28:46 -0500

    Create loading state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { BsTwitter } from 'react-icons/bs';
 import { RiGitRepositoryFill } from 'react-icons/ri';
 import { MdPeopleAlt } from 'react-icons/md';
+import { Bars } from 'react-loader-spinner';
 import './App.css';
 
 function App() {
@@ -10,42 +11,59 @@ function App() {
 
   const getUser = async () => {
     const data = await fetch(' https://api.github.com/users/sammwyy').then( response => response.json());
-    setUser(data);
+
+    setUser(data)
+    
   }
 
   useEffect( () => {
     getUser();
   },[])
 
+  
   return (
     <div className="App">
       <div className="card">
-        <div className="photo-profile">
-            <img src={user?.avatar_url} alt="profile" />
-        </div>
-        <div className="content-profile">
-            <div className="name">{user?.name}</div>
-            <div className="bio">{user?.bio}</div>
-            <div className="recounts">
-                <div className="followers">
-                    <MdPeopleAlt />
-                    {user?.followers}
-                </div>
-                <div className="repos">
-                    <RiGitRepositoryFill />
-                    {user?.public_repos}
-                </div>
-                {(user?.twitter_username) && (
-                    <div className="twitter">
-                        <a target="__blank"
-                            href={'https://twitter.com/' + user?.twitter_username}>
-                            <BsTwitter />
-                            {user?.twitter_username}
-                        </a>
-                    </div>
-                )}
+        { !user.hasOwnProperty('id') ? 
+          (<Bars
+            height="80"
+            width="80"
+            color="#9AD2CB"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />) : 
+          (<>
+            <div className="photo-profile">
+              <img src={user?.avatar_url} alt="profile" />
             </div>
-        </div>
+            <div className="content-profile">
+                <div className="name">{user?.name}</div>
+                <div className="bio">{user?.bio}</div>
+                <div className="recounts">
+                    <div className="followers">
+                        <MdPeopleAlt />
+                        {user?.followers}
+                    </div>
+                    <div className="repos">
+                        <RiGitRepositoryFill />
+                        {user?.public_repos}
+                    </div>
+                    {(user?.twitter_username) && (
+                        <div className="twitter">
+                            <a target="__blank"
+                                href={'https://twitter.com/' + user?.twitter_username}>
+                                <BsTwitter />
+                                {user?.twitter_username}
+                            </a>
+                        </div>
+                    )}
+                </div>
+            </div>
+          </>)
+        }
+        
       </div>
     </div>
   );
