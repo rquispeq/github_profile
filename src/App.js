5ac7735f67f878df307b8b67017dedbc90a25522
Rony Quispe <oghmand@gmail.com>
2022-11-13 20:34:42 -0500

    Format code

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ function App() {
   const [user, setUser] = useState({});
 
   const getUser = async () => {
-    const data = await fetch(' https://api.github.com/users/sammwyy').then( response => response.json());
+    const data = await fetch(' https://api.github.com/users/sammwyy').then(response => response.json());
 
     setUser(data)
-    
+
   }
 
-  useEffect( () => {
+  useEffect(() => {
     getUser();
-  },[])
+  }, [])
 
-  
+
   return (
     <div className="App">
       <div className="card">
-        { !user.hasOwnProperty('id') ? 
+        {!user.hasOwnProperty('id') ?
           (<Bars
             height="80"
             width="80"
@@ -33,37 +33,37 @@ function App() {
             wrapperStyle={{}}
             wrapperClass=""
             visible={true}
-          />) : 
+          />) :
           (<>
             <div className="photo-profile">
               <img src={user.avatar_url} alt="profile" />
             </div>
             <div className="content-profile">
-                <div className="name">{user.name}</div>
-                <div className="bio">{user.bio}</div>
-                <div className="recounts">
-                    <div className="followers">
-                        <MdPeopleAlt />
-                        {user.followers}
-                    </div>
-                    <div className="repos">
-                        <RiGitRepositoryFill />
-                        {user.public_repos}
-                    </div>
-                    {(user.twitter_username) && (
-                        <div className="twitter">
-                            <a target="__blank"
-                                href={'https://twitter.com/' + user.twitter_username}>
-                                <BsTwitter />
-                                {user.twitter_username}
-                            </a>
-                        </div>
-                    )}
+              <div className="name">{user.name}</div>
+              <div className="bio">{user.bio}</div>
+              <div className="recounts">
+                <div className="followers">
+                  <MdPeopleAlt />
+                  {user.followers}
                 </div>
+                <div className="repos">
+                  <RiGitRepositoryFill />
+                  {user.public_repos}
+                </div>
+                {(user.twitter_username) && (
+                  <div className="twitter">
+                    <a target="__blank"
+                      href={'https://twitter.com/' + user.twitter_username}>
+                      <BsTwitter />
+                      {user.twitter_username}
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
           </>)
         }
-        
+
       </div>
     </div>
   );
