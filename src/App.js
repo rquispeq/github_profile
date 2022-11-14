a63813989ebd04456194bbf04226ff42609e3115
Rony Quispe <oghmand@gmail.com>
2022-11-13 19:05:57 -0500

    Add icons to recounts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
+import { BsTwitter } from 'react-icons/bs';
+import { RiGitRepositoryFill } from 'react-icons/ri';
+import { MdPeopleAlt } from 'react-icons/md';
 import './App.css';
-import logo from './logo.svg'
 
 function App() {
 
@@ -26,15 +28,22 @@ function App() {
             <div className="bio">{user?.bio}</div>
             <div className="recounts">
                 <div className="followers">
+                    <MdPeopleAlt />
                     {user?.followers}
                 </div>
                 <div className="repos">
+                    <RiGitRepositoryFill />
                     {user?.public_repos}
                 </div>
-                {(user?.twitter_username) && (<div className="twitter">
-                    <a target="__blank" href={'https://twitter.com/' + user?.twitter_username}>@{user?.twitter_username}</a>
-                </div>)}
-                
+                {(user?.twitter_username) && (
+                    <div className="twitter">
+                        <a target="__blank"
+                            href={'https://twitter.com/' + user?.twitter_username}>
+                            <BsTwitter />
+                            {user?.twitter_username}
+                        </a>
+                    </div>
+                )}
             </div>
         </div>
       </div>
