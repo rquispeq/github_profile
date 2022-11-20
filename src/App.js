5380c18605571c0c92fe211288086a07d6288a10
Rony Quispe <oghmand@gmail.com>
2022-11-20 16:54:23 -0500

    format code

--- src/App.js
+++ src/App.js
@@ -4,106 +4,106 @@ import { RiGitRepositoryFill } from 'react-icons/ri'
 import { MdPeopleAlt } from 'react-icons/md'
 import { FaSearch } from 'react-icons/fa'
 import { Bars } from 'react-loader-spinner'
-import Select, {components, DropdownIndicatorProps } from 'react-select'
+import Select, { components, DropdownIndicatorProps } from 'react-select'
 import './App.css'
 
 function App() {
 
-  const [user, setUser] = useState({});
-  const [listUsers, setListUsers] = useState([]);
-  const [search, setSearch] = useState('');
+    const [user, setUser] = useState({});
+    const [listUsers, setListUsers] = useState([]);
+    const [search, setSearch] = useState('');
 
-  const getUser = async () => {
-    const data = await fetch('https://api.github.com/users/sammwyy')
-        .then(response => response.json());
-    setUser(data)
-  }
-
-  const handleSearch = (text) => {
-    if(typeof text === 'string' && text.length > 2) {
-        setSearch(text)
+    const getUser = async () => {
+        const data = await fetch('https://api.github.com/users/sammwyy')
+            .then(response => response.json());
+        setUser(data)
     }
-  }
 
-
-  const searchUser = async (text) => {
-    // console.log('texto busqueda',text)
-    if (text.length > 2 && typeof text === 'string') {
-        const responseData = await fetch(`https://api.github.com/search/users?q=${text}`)
-            .then(response => response.json())
-            // .then(data => setListUsers(data.items));
-        // return data
-        const githubUsers = responseData.items.map( (githubUser) => {
-            return {value: githubUser.login, label: githubUser.login}
-        })
-        setListUsers(githubUsers)
-    } else {
-        setListUsers([])
-    }
-  }
-
-  useEffect(() => {
-    getUser();
-  }, [])
-
-  useEffect(() => {
-    searchUser(search)
-  },[search])
-
-
-  return (
-    <div className="App">
-        <div className="search">
-            <Select
-                onInputChange={(e) => handleSearch(e)}
-                options={listUsers}
-                placeholder="Buscar usuario"
-            />
-        </div>
-      <div className="card">
-        {!user.hasOwnProperty('id') ?
-          (<Bars
-            height="80"
-            width="80"
-            color="#9AD2CB"
-            ariaLabel="bars-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />) :
-          (<>
-            <div className="photo-profile">
-              <img src={user.avatar_url} alt="profile" />
-            </div>
-            <div className="content-profile">
-              <div className="name">{user.name}</div>
-              <div className="bio">{user.bio}</div>
-              <div className="recounts">
-                <div className="followers">
-                  <MdPeopleAlt />
-                  {user.followers}
-                </div>
-                <div className="repos">
-                  <RiGitRepositoryFill />
-                  {user.public_repos}
-                </div>
-                {(user.twitter_username) && (
-                  <div className="twitter">
-                    <a target="__blank"
-                      href={'https://twitter.com/' + user.twitter_username}>
-                      <BsTwitter />
-                      {user.twitter_username}
-                    </a>
-                  </div>
-                )}
-              </div>
-            </div>
-          </>)
+    const handleSearch = (text) => {
+        if (typeof text === 'string' && text.length > 2) {
+            setSearch(text)
         }
+    }
 
-      </div>
-    </div>
-  );
+
+    const searchUser = async (text) => {
+        // console.log('texto busqueda',text)
+        if (text.length > 2 && typeof text === 'string') {
+            const responseData = await fetch(`https://api.github.com/search/users?q=${text}`)
+                .then(response => response.json())
+            // .then(data => setListUsers(data.items));
+            // return data
+            const githubUsers = responseData.items.map((githubUser) => {
+                return { value: githubUser.login, label: githubUser.login }
+            })
+            setListUsers(githubUsers)
+        } else {
+            setListUsers([])
+        }
+    }
+
+    useEffect(() => {
+        getUser();
+    }, [])
+
+    useEffect(() => {
+        searchUser(search)
+    }, [search])
+
+
+    return (
+        <div className="App">
+            <div className="search">
+                <Select
+                    onInputChange={(e) => handleSearch(e)}
+                    options={listUsers}
+                    placeholder="Buscar usuario"
+                />
+            </div>
+            <div className="card">
+                {!user.hasOwnProperty('id') ?
+                    (<Bars
+                        height="80"
+                        width="80"
+                        color="#9AD2CB"
+                        ariaLabel="bars-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                    />) :
+                    (<>
+                        <div className="photo-profile">
+                            <img src={user.avatar_url} alt="profile" />
+                        </div>
+                        <div className="content-profile">
+                            <div className="name">{user.name}</div>
+                            <div className="bio">{user.bio}</div>
+                            <div className="recounts">
+                                <div className="followers">
+                                    <MdPeopleAlt />
+                                    {user.followers}
+                                </div>
+                                <div className="repos">
+                                    <RiGitRepositoryFill />
+                                    {user.public_repos}
+                                </div>
+                                {(user.twitter_username) && (
+                                    <div className="twitter">
+                                        <a target="__blank"
+                                            href={'https://twitter.com/' + user.twitter_username}>
+                                            <BsTwitter />
+                                            {user.twitter_username}
+                                        </a>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </>)
+                }
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
